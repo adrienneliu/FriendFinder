29a6bdb712f1c17bc354645519ce9f89b2442f19
Adrienne <iliu.adrienne@gmail.com>
2019-06-29 18:43:19 -0700

    add routes to survey and home

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
+// Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
+
 var express = require("express"); 
 var path = require("path"); 
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080; 
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+
+// require("/.routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT http://localhost:" + PORT);
+});
