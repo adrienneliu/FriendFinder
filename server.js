e6d03b5175790f8a391ca94e6d0e6eb83790bd41
Adrienne <iliu.adrienne@gmail.com>
2019-06-30 23:38:33 -0700

    add some styling to home and survey html pages

--- server.js
+++ server.js
@@ -7,21 +7,13 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080; 
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// Expose the public directory to access CSS files
-app.use(express.static(path.join(__dirname, './app/public')));
-
-// Add middleware for parsing incoming request bodies
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
-// require("/.routing/apiRoutes")(app);
+// Routes
+require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
 
