d6a006cef9bc67becf3dae236750b95d0433d4fa
Adrienne <iliu.adrienne@gmail.com>
2019-07-01 23:55:27 -0700

    add questions and resultsarray

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,19 @@
 var resultsArray = [
     {
         name: "India", 
-        scores: ["1","4"]
+        scores: ["1","4", "3", "4", "4", "2", "1", "2"]
     },
     {
         name: "Italy",
-        scores: ["3", "2"]
+        scores: ["3", "2", "1", "1", "3", "4", "2", "3"]
     }, 
     {
         name: "Taiwan", 
-        scores: ["4", "3"]
+        scores: ["4", "3", "2", "2", "1", "3", "4", "1"]
+    }, 
+    {
+        name: "Stay home", 
+        scores: ["2","1", "4", "3", "2", "1", "3", "4"]
     }
 ];
 
